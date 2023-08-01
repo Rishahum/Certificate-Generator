22c3d5979fad9cb1b93318702e0bb317b376298d
Risha Sinha <98969301+Rishahum@users.noreply.github.com>
2023-08-01 16:42:15 +0530

    Add files via upload

---
+++ it/static/index.js
@@ -0,0 +1,17 @@
+var tl = gsap.timeline();
+
+tl.from(".nav>i, .nav>div, .head, .content, .img1",{
+    y:-200,
+    duration:1,
+    delay:0,
+    opacity:0,
+    stagger:0.2
+})
+signBtn=document.querySelector(".sign_in")
+signBtn.addEventListener("click", () => {
+    location.assign("./static/sign.html");
+  });
+Btn=document.querySelector(".button")
+Btn.addEventListener("click", () => {
+    location.assign("/certificate-us");
+  });
